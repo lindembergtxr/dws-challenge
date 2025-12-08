ff2d557138897531b5a32814a40514c783c387e5
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-12-08 16:19:40 -0300

    feat: initial style configurations
    Add logo to assets folder
    Create global style and add to app
    Configure Open Sans font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 import { theme } from './app.styles'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Open Sans', sans-serif;
+  }
+`
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <h1>Hello, World!</h1>
     </ThemeProvider>
   )
