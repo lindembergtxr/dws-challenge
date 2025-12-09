05eb92d87fca09f7664046946e0739500cc48ba1
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-12-09 16:33:54 -0300

    feat: add react query and configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { theme } from './styles/app.styles'
 import { BackgroundBlur } from './components/backgroundBlur'
+import { HomePage } from './pages/home'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -18,12 +22,23 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BackgroundBlur />
-      <h1>Hello, World!</h1>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <BackgroundBlur />
+
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
